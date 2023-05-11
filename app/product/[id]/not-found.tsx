94a6fbee7b7dfd72a8d879428f05a73c7da523f7
LucioFider <fidelislucio404@gmail.com>
2023-05-11 11:08:35 +1000

    Implemented intercepting routes

--- app/product/[id]/not-found.tsx
+++ app/product/[id]/not-found.tsx
@@ -1,17 +1,8 @@
-import Link from "next/link";
-import React from "react";
-
-const NotFound = () => {
+export default function NotFound() {
   return (
-    <div className="mt-24 text-center">
+    <div className="mt-24">
       <h2>Not Found</h2>
-      <p>Cound not find requested resource</p>
-
-      <Link href="/" className="text-blue-500 cursor-pointer mt-20">
-        Go back to home page!
-      </Link>
+      <p>Could not find requested resource</p>
     </div>
   );
-};
-
-export default NotFound;
+}
